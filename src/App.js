14ea2c370247475bc4fc92b76fc26031601c7463
Anqlem <Anqlem@Anqlem-PC.lan>
2022-10-17 12:58:01 +0300

    Little changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Table from "./components/Table/Table";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Details from "./components/Details/DetailsPage";
-import tbl from "../src/components/Table/Table.module.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className={tbl.app}>
+      <div>
         <Routes>
           <Route path="" element={<Table />} />
           <Route path="/details/:id" element={<Details />} />
